ecbf83804e0825770d1198d1531305e75fa377cf
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2015-10-20 11:49:36 +0600

    More experiments

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
-var canvas, ctx, source, audioCtx, analyser, freqArray, bars, barX, barW, barH;
+var canvas, canvas1, ctx, ctx1, source, audioCtx, analyser, freqArray, bars, barX, barW, barH;
 
+var colors = ['#A1D490', '#90C3D4', '#D4A190', '#C390D4'];
 var audio = new Audio();
 audio.src = 'http://localhost:3000/song';
 audio.crossorigin = 'anonymous';
@@ -19,11 +20,15 @@ function init() {
   canvas = document.getElementById('audio_bars_render');
   ctx = canvas.getContext('2d');
 
+  canvas1 = document.getElementById('particles_render');
+  ctx1 = canvas1.getContext('2d');
+
   source = audioCtx.createMediaElementSource(audio);
   source.connect(analyser);
   analyser.connect(audioCtx.destination);
 
   animate();
+  animateParticles();
 }
 
 function animate() {
@@ -33,7 +38,6 @@ function animate() {
   analyser.getByteFrequencyData(freqArray);
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = '#A1D490';
 
   bars = 100;
   for (var i = 0; i < bars; i++) {
@@ -41,6 +45,20 @@ function animate() {
     barW = 2;
     barH = - (freqArray[i] / 2);
 
+    ctx.fillStyle = _.sample(colors);
     ctx.fillRect(barX, canvas.height, barW, barH);
   }
+}
+
+function animateParticles() {
+  var fps = 10;
+
+  setTimeout(function () {
+    window.requestAnimationFrame(animateParticles);
+
+    ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
+
+    ctx1.fillStyle = _.sample(colors);
+    ctx1.fillRect(_.random(0, canvas1.width), _.random(0, canvas1.height), 10, 10);
+  }, 1000 / fps);
 }
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Audio Experiments' });
+  res.render('index', { title: 'Audio Experiments', song: 'Duke Dumont - Ocean Drive' });
 });
 
 module.exports = router;
